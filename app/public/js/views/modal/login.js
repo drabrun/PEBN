258ca9ff8f14dbe5c8a7914efc6854f97c354f95
drabrun <drabrun@gmail.com>
2014-08-28 09:20:30 -0400

    pre init

--- app/public/js/views/modal/login.js
+++ app/public/js/views/modal/login.js
@@ -1,10 +1,36 @@
 LoginView = Backbone.Marionette.ItemView.extend({
 	events: {
-		"click #submitLogin" : "submitLogin"
+		"click #submitLogin" : "submitLogin",
+		"click #loginAlertClose" : "hideAlert",
+		"keyup input" : "enterSubmit"
+	},
+	inputChanged : function(e){
+	
 	},
     template: "login",
-    submitLogin: function(){
-    	App.request("ModalManager").close()
+    enterSubmit : function(evt){
+		if(evt.keyCode == 13){
+        	this.$("#submitLogin").click();
+    	}
+    },
+    submitLogin: function(evt){
+    	var username = $("#loginUsernameInput").val();
+    	var password = $("#loginPasswordInput").val();
+    	
+    	if(password == null || username == null 
+    	   || password.length == 0 || username.length == 0){
+    		this.showAlert('Username & Password Required');
+    	   }
+    	else{
+    		App.request("ModalManager").close()
+    	}
+    },
+    showAlert : function(message){
+    	$("#loginAlertMsg").html(message);
+		$("#alertBox").removeClass("hide");
+    },
+    hideAlert : function(){
+    	$("#alertBox").addClass("hide");
     },
     render: function() {
         var that = this;
